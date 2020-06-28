911e9945ee573d3ee78d1f225e96c3a4100e1402
reflective-partial-8357 <scarter31191@gmail.com>
2020-06-28 21:33:31 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,7 +14,7 @@ require 'pry'
    }
   
 def remove_strawberry(contacts)
-  contacts.each do |person, contact_deatails_hash|
+  contacts.each do |person, contact_details_hash|
     binding.pry
   end
 
