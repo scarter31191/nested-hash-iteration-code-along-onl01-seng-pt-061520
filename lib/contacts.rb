bbb8cf8eca1c594cf385f6e68fe1ce71b9a645dd
reflective-partial-8357 <scarter31191@gmail.com>
2020-06-28 21:47:45 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,10 +18,12 @@ def remove_strawberry(contacts)
     if person == "Freddy Mercury"
       contact_details_hash.each do |attribute, data|
         if attribute == :favorite_ice_cream_flavors
-    binding.pry
+          data.delete_if
+          {|ice_cream| ice_cream == "strawberry"}
+          end
+        end
+      end
     end
   end
-end
-end
 end
 
